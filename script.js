8286fcffb80386c9f992c2aed1f7786dcbb168a2
chartaseus <90235896+chartaseus@users.noreply.github.com>
2023-10-09 12:13:19 +0700

    Create playRound function

--- script.js
+++ script.js
@@ -10,3 +10,56 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+// create a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection
+function playRound(playerSelection, computerSelection) {
+
+    // Make your function’s playerSelection parameter case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+    
+    // Throw error for invalid input
+    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+        return 'Invalid input. Enter rock, paper, or scissors.'
+    }
+    /* Possible combinations:
+        player   >< computer
+        --------------------
+        rock     >< rock     => tie
+        rock     >< paper    => lose
+        rock     >< scissors => win
+        paper    >< rock     => win
+        paper    >< paper    => tie
+        paper    >< scissors => lose
+        scissors >< rock     => lose
+        scissors >< paper    => win
+        scissors >< scissors => tie
+    */
+
+    // If player selection = computer selection then it's a tie
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+
+    // rock >< paper, paper >< scissors, scissors >< rock => lose
+    if (
+        (playerSelection === "rock" && computerSelection === "paper")
+        || (playerSelection === "paper" && computerSelection === "scissors")
+        || (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+    // rock >< scissors, paper >< rock, scissors >< paper => win
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors")
+        || (playerSelection === "paper" && computerSelection === "rock")
+        || (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
+
+// Testing playRound
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
